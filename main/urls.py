1785316aa9f14d10f8e9de2f293f1ad0955fbb9c
Vlad M <vladpochta5@gmail.com>
2017-07-22 20:51:21 +0300

    add route for index and detail view

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,8 @@ from main import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    # /pools/how-old-are-you
+    url(r'^pools/(?P<slug>[\w\-]+)/$',
+        views.DetailView.as_view(),
+        name="detail"),
 ]
