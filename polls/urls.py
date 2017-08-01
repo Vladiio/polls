ba9b2c79d455f6dc0c2b25e7b424759dec5bf837
Vlad M <vladpochta5@gmail.com>
2017-08-01 17:31:19 +0300

    Provide login functionality

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 from questions.views import QuestionListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', QuestionListView.as_view(), name='home'),
-    url(r'^', include('questions.urls', namespace='questions')),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^about/$', TemplateView.as_view(
+            template_name='about.html'), name='about'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^questions/', include('questions.urls', namespace='questions')),
 ]
